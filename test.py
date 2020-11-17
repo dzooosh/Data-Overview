dcf60c9d37f1ceb30f9b8cf93533e82dad5abd9e
dzooosh <37011052+dzooosh@users.noreply.github.com>
2020-11-17 14:55:06 +0100

    Create test.py
    Simple addition and square multiplier

---
+++ test.py
@@ -0,0 +1,16 @@
+# This is a simple calculator
+# Display this after evaluation
+
+#create function for all operations
+function Add(x, y):
+  result = x + y
+  return result
+function sqrt(x):
+   result = x*x
+   return result
+   
+x = input('enter your x value': )
+y = input('enter your y value': )
+ 
+sqrt(y)
+Add(x, y)
